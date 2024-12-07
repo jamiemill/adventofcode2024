19e0d3e3d03d88efa36d01df0a671ddc4da2f674
Jamie Mill <jamiermill@gmail.com>
2024-12-07 16:55:10 +0100

    cleanup

--- day02.py
+++ day02.py
@@ -2,6 +2,10 @@ def parse(input_data:str) ->list[list[int]]:
     lines = input_data.splitlines()
     return [list(map(int, l.split())) for l in lines]
 
+def range_ok(pair):
+    [l,r] = pair
+    return abs(l-r) < 4 and abs(l-r) > 0
+
 
 def solve(input_data:str) -> int:
     reports = parse(input_data)
@@ -9,16 +13,9 @@ def solve(input_data:str) -> int:
     for report in reports:
         is_increasing = sorted(report) == report
         is_decreasing = list(reversed(sorted(report))) == report
-        differences_are_ok = True
-        pairs = zip(report, report[1:])
-        for pair in pairs:
-            [l,r] = pair
-            if abs(l-r) > 3:
-                differences_are_ok = False
-            if abs(l-r) < 1:
-                differences_are_ok = False
-        is_safe = (is_increasing or is_decreasing) and differences_are_ok
-        if is_safe:
+        pairs = list(zip(report, report[1:]))
+        differences_are_ok = False not in map(range_ok, pairs)
+        if (is_increasing or is_decreasing) and differences_are_ok:
             count_safe += 1
     return count_safe
 
