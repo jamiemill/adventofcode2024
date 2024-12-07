6ae4c5264f04ac17830f0dfd6eb0c0d8bfc4d226
Jamie Mill <jamiermill@gmail.com>
2024-12-07 21:20:35 +0100

    tidy

--- day03.py
+++ day03.py
@@ -1,7 +1,11 @@
 import re
 
+CMD_MUL = r"mul\(\d+,\d+\)"
+CMD_DO = r"do\(\)"
+CMD_DONT = r"don't\(\)"
+
 def solve(input_data:str) -> int:
-    matches = re.findall(r"mul\(\d+,\d+\)", input_data)
+    matches = re.findall(CMD_MUL, input_data)
     total = 0
     for match in matches:
         l,r = re.findall(r"\d+", match)
@@ -9,13 +13,13 @@ def solve(input_data:str) -> int:
     return total
 
 def solve_part2(input_data:str) -> int:
-    matches = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", input_data)
+    matches = re.findall(CMD_MUL + "|" + CMD_DO + "|" + CMD_DONT, input_data)
     enabled = True
     total = 0
     for match in matches:
-        if match == "do()": enabled = True
-        if match == "don't()": enabled = False
-        if re.match(r"mul\(\d+,\d+\)", match) and enabled:
+        if re.match(CMD_DO, match): enabled = True
+        if re.match(CMD_DONT, match): enabled = False
+        if re.match(CMD_MUL, match) and enabled:
             l,r = re.findall(r"\d+", match)
             total += int(l) * int(r)
     return total
