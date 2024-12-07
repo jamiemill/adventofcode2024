b2e645de5254eacef49577c5182da1944bf618a0
Jamie Mill <jamiermill@gmail.com>
2024-12-07 21:10:39 +0100

    day 3 part 2

--- day03.py
+++ day03.py
@@ -8,6 +8,17 @@ def solve(input_data:str) -> int:
         total += int(l) * int(r)
     return total
 
+def solve_part2(input_data:str) -> int:
+    matches = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", input_data)
+    enabled = True
+    total = 0
+    for match in matches:
+        if match == "do()": enabled = True
+        if match == "don't()": enabled = False
+        if re.match(r"mul\(\d+,\d+\)", match) and enabled:
+            l,r = re.findall(r"\d+", match)
+            total += int(l) * int(r)
+    return total
     
 
 if __name__ == "__main__":
@@ -15,5 +26,5 @@ if __name__ == "__main__":
         puzzle_input = f.read()
     result = solve(puzzle_input)
     print(f"Result: {result}")
-    # result = solve_part2(puzzle_input)
-    # print(f"Result: {result}")
+    result = solve_part2(puzzle_input)
+    print(f"Result: {result}")
--- day03_test.py
+++ day03_test.py
@@ -1,11 +1,12 @@
 import pytest
-from day03 import solve #, solve_part2
+from day03 import solve, solve_part2
 
 
 def test_example():
     example_input = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
     assert solve(example_input) == 161
-    # assert solve_part2(example_input) == 4
+    example_input2 = """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
+    assert solve_part2(example_input2) == 48
 
 
 test_example()
