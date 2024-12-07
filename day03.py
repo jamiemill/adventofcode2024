ff8c80b3bc67889c9d77c77f048359bb84b241ad
Jamie Mill <jamiermill@gmail.com>
2024-12-07 21:40:54 +0100

    clean

--- day03.py
+++ day03.py
@@ -4,24 +4,26 @@ CMD_MUL = r"mul\(\d+,\d+\)"
 CMD_DO = r"do\(\)"
 CMD_DONT = r"don't\(\)"
 
+def multiply_nums_in_str(str):
+    l,r = re.findall(r"\d+", str)
+    return int(l) * int(r)
+
 def solve(input_data:str) -> int:
     matches = re.findall(CMD_MUL, input_data)
     total = 0
     for match in matches:
-        l,r = re.findall(r"\d+", match)
-        total += int(l) * int(r)
+        total += multiply_nums_in_str(match)
     return total
 
 def solve_part2(input_data:str) -> int:
-    matches = re.findall(CMD_MUL + "|" + CMD_DO + "|" + CMD_DONT, input_data)
+    matches = re.findall(f"{CMD_MUL}|{CMD_DO}|{CMD_DONT}", input_data)
     enabled = True
     total = 0
     for match in matches:
         if re.match(CMD_DO, match): enabled = True
         if re.match(CMD_DONT, match): enabled = False
         if re.match(CMD_MUL, match) and enabled:
-            l,r = re.findall(r"\d+", match)
-            total += int(l) * int(r)
+            total += multiply_nums_in_str(match)
     return total
     
 
